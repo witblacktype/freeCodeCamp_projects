e2f5c6e3d8bddc24c4e31e8403f02b24ef9e2a67
Bretton Raymond <bretton.raymond@gmail.com>
2015-07-22 10:26:00 -0700

    Add Bonfires directory. Add two projects to Bonfires directory.

---
+++ bonfires/reverse-a-string/logic.js
@@ -0,0 +1,27 @@
+/* Bonfire: Reverse a String */
+
+function reverseString(str) {
+
+/* Solution 1 */
+/*
+	str = str.split('').reverse().join('');
+	console.log(str);
+  return str;
+*/
+
+/* Solution 2 */
+	var storage = [];
+	for(var i = str.length; i > 0; i--){
+		storage[i] = str.charAt(str.length - i);
+	}
+	str = storage.join(" ");
+	var output = new String();
+	for(var i = 0; i < str.length; i++){
+		var space = " ";
+		if(str.charAt(i) != space){
+			output = output.concat(str.charAt(i));
+		}
+	}
+	return output;
+};
+reverseString("hello");
