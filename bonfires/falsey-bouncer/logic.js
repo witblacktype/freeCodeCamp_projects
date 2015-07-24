1601b38c8dae56e762a656c63e8b4af132fc774f
Bretton Raymond <bretton.raymond@gmail.com>
2015-07-24 01:17:56 -0700

    Add new project to bonfires directory.

---
+++ bonfires/falsey-bouncer/logic.js
@@ -0,0 +1,18 @@
+/* Bonfire: falsey bouncer */
+
+function bouncer(arr) {
+
+
+	function falsey(){
+
+	}
+
+  var b = arr.filter(falsey);
+  
+
+
+
+  return arr;
+}
+
+bouncer([7, 'ate', '', false, 9]);
--- bonfires/mutations/logic.js
+++ bonfires/mutations/logic.js
@@ -1,26 +1,18 @@
 /* Bonfire: Mutations */
 
 function mutation(arr) {
-	
+	var match = false;
 	for (var i = 0; i < arr[1].length; i++){
+		match = false;
 		console.log(arr[1].charAt(i));
-
-		for(var j = 0; j < arr[0].length; j++){
-			if(arr[1].charAt(i) === arr[0].charAt(j)){
-				console.log("a match");
-				console.log(j);
-				j = arr[0].length;
-				console.log(j);
-			}
-			else{
-				console.log(false);
-				return false;
+		for(var j = 0; j < arr[0].length; j++){	
+			console.log(arr[0].charAt(j).toLowerCase());
+			if(arr[1].charAt(i).toLowerCase() == arr[0].charAt(j).toLowerCase()){
+				match = true;
 				break;
 			}
-		}		
+		}	
 	}
-	console.log(true);
-  return true;
+	return match;
 }
-
-mutation(['hello', 'hey']);
+mutation(['hello', 'Hey']);
