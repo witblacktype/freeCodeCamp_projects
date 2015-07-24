7b5768c15b92679d4f3c44f8d4dda852f41ded27
Bretton Raymond <bretton.raymond@gmail.com>
2015-07-23 17:00:33 -0700

    Edit content in bonfires.

--- bonfires/check-for-palindromes/logic.js
+++ bonfires/check-for-palindromes/logic.js
@@ -2,9 +2,8 @@
 
 function palindrome(str) {
 	function reverse(match){
-		console.log(match.split('').reverse().join('') + " 3.5");
 		return match.split('').reverse().join('');
-	}
+  }
   str = str.toLowerCase();
   str = str.replace(/[\s\W]/g, '');
 	var re = str.replace(/[a-zA-z]+/i, reverse);
