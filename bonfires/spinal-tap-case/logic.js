5e41809dab5e84f8fe3c7346a052a4b57d1a6afa
Bretton Raymond <bretton.raymond@gmail.com>
2015-08-10 15:55:05 -0700

    Add new project to Bonfires Directory.

--- bonfires/spinal-tap-case/logic.js
+++ bonfires/spinal-tap-case/logic.js
@@ -1,9 +1,12 @@
 /* Bonfire: Spinal Tap Case */
 
 function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
+	function lowercase(match){
+		return match.toLowerCase();
+	}
+	str = str.replace(/[ _]/gi, "-");
+	str = str.replace(/([a-z])([A-Z])/g, "$1-$2");
+	str = str.replace(/[A-Z]/g, lowercase);
   return str;
 }
-
-spinalCase('This Is Spinal Tap');
+spinalCase('thisIsSpinalTap');
---
+++ bonfires/sum-all-odd-fibonacci-numbers/logic.js
@@ -0,0 +1,18 @@
+/* Bonfire: Sum All Odd Fibonacci Numbers */
+
+function sumFibs(num) {
+
+	var f1 = 1;
+	var f2 = 1;
+
+	function fib(){
+		
+	}
+
+
+
+
+  return num;
+}
+
+sumFibs(4);
