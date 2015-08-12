b8b1c9a9b6cced5178fb90b2a52a200872b59beb
Bretton Raymond <bretton.raymond@gmail.com>
2015-08-12 21:37:25 +0000

    Finish looping-style Fibonacci solution

--- bonfires/sum-all-odd-fibonacci-numbers/logic.js
+++ bonfires/sum-all-odd-fibonacci-numbers/logic.js
@@ -1,16 +1,24 @@
 /* Bonfire: Sum All Odd Fibonacci Numbers */
 
 function sumFibs(num) {
-
-	var tmp = num;
-	while (num > 2){
-		
+	var oddFibs = function(n){
+		var sum = 0;
+		var a = 0, b = 1, f = 1;
+		console.log("F(1) = " + f);
+		for (var i = 2; f <= n; i++){
+			if(f%2 === 1){
+				sum += f;
+			}
+			f = a + b;
+			a = b;
+			b = f;
+			console.log("F(" +  i + ") = " + f);
+		}
+		console.log("final value for f = " + f);
+		console.log("sum = " + sum);
+		return sum;
 	}
-
-
-
-
-  return num;
+  return oddFibs(num);
 }
-
+console.log(sumFibs(4));
 sumFibs(4);
