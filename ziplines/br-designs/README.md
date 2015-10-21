1216076d73e9cbccf351f1d24bd8178682807648
Bretton Raymond <bretton.raymond@gmail.com>
2015-10-21 16:26:19 -0700

    BR-Designs Project Progress. Card Interaction Complete. Layout major changes finished.

---
+++ ziplines/br-designs/README.md
@@ -0,0 +1,8 @@
+# Readme File
+
+Subtle Patterns
+Specific license terms available on the website do not fall under Creative Commons License. Requires Attribution. Not able to provide file for download.
+
+Tile Patterns
+http://tiled-bg.blogspot.com/
+CC License. Requires Attribution.
---
+++ ziplines/br-designs/js/app.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+  $(window).load(function(){
+    var h1 = $('.card-img').height();
+    var h2 = $('.banner').innerHeight();
+    var height = h1 + h2;
+    $('.card').css('height', height + 'px' );
+    $('.card-text').css('height', height + 'px');
+  });
+  $(window).resize(function(){
+    var h1 = $('.card-img').innerHeight();
+    var h2 = $('.banner').innerHeight();
+    var height = h1 + h2;
+    $('.card').css('height', height + 'px' );
+    $('.card-text').css('height', height + 'px');
+    $('.card-text').css('margin-top', '0px');
+    $('.card-detils').animate({
+      opacity: 1
+      }, 500, function(){
+        //animation callback
+    });
+  });
+  // Card Controller
+  $('.card-text').click(function(){
+    /* Collapse other open cards */
+    $('.card-details.open').css('margin-top', '0px');
+    /* Animation to Move all text content into view */
+    $('.card-text.open').css('margin-top', '0px');
+    $('.card-text.open').find('.card-details').animate({
+        opacity: 0
+    }, 0, function(){
+      //animation callback
+    });
+    var $control = $('.card-text.open').find('.fa-angle-double-up');
+    $({deg: -180}).animate({deg: 0}, {
+        duration: 500,
+        step: function(now) {
+            // in the step-callback (that is fired each step of the animation),
+            // you can use the `now` paramter which contains the current
+            // animation-position (`0` up to `angle`)
+            $control.css({
+                transform: 'rotate(' + now + 'deg)'
+            });
+        },
+        delay: 450
+    });
+    $('.card-text.open').removeClass('open');
+
+
+    if($(this).css('margin-top') != '0px'){
+      $(this).css('margin-top', '0px');
+      var $elem = $(this).find('.fa-angle-double-up');
+      $({deg: -180}).animate({deg: 0}, {
+          duration: 500,
+          step: function(now) {
+              // in the step-callback (that is fired each step of the animation),
+              // you can use the `now` paramter which contains the current
+              // animation-position (`0` up to `angle`)
+              $elem.css({
+                  transform: 'rotate(' + now + 'deg)'
+              });
+          },
+          delay: 450
+      });
+      $(this).find('.card-details').animate({
+          opacity: 0
+      }, 0, function(){
+        //animation callback
+      });
+      $(this).removeClass('open');
+    }
+    else{
+      $(this).addClass('open');
+      var height = $(this).siblings('.card-img').height();
+      $(this).css('margin-top', '-' + height + 'px');
+      var $elem = $(this).find('.fa-angle-double-up');
+      $({deg: 0}).animate({deg: -180}, {
+          duration: 500,
+          step: function(now) {
+              $elem.css({
+                  transform: 'rotate(' + now + 'deg)'
+              });
+          },
+          delay: 450
+      });
+      $(this).find('.card-details').animate({
+          opacity: 1
+      }, 500, function(){
+        //animation callback
+      });
+    }
+  });
+});
